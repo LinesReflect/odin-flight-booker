9a7e42c29283aa19eaf4e4f667a120c2e0c59a04
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-02-25 13:00:50 -0600

    Allow flights to be searched for with specific parameters and selected to be booked

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+
+    @searched_flights = Flight.where(departure_airport_id: params[:departure_airport_id], arrival_airport_id: params[:arrival_airport_id])
   end
 end
