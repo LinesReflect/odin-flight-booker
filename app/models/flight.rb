8e37cbdece3fb228ad479f74a78322cc86e15d73
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-03-17 12:16:08 -0500

    Add methods to help display the flight search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,16 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, foreign_key: "departure_airport_id", class_name: "Airport"
   belongs_to :arrival_airport, foreign_key: "arrival_airport_id", class_name: "Airport"
+
+  def format_departure_date
+    self.departure_date.strftime("%A, %B %d, %Y")
+  end
+
+  def self.distinct_days
+    flights_distinct_days = []
+    Flight.all.each do |f|
+      flights_distinct_days.push(f) unless flights_distinct_days.any? { |added_flight| added_flight.departure_date.to_date == f.departure_date.to_date }
+    end
+    flights_distinct_days
+  end
 end
