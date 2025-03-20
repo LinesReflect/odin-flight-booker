51f63f9d3c5a1b118c30eb1a1e1f9d1c5afb9978
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-03-20 13:48:16 -0500

    Refactor code to cover cases where a flight search has not been done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,9 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @flight_search_results = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND departure_date LIKE ?",
-       params[:departure_airport], params[:arrival_airport], params[:departure_date] + "%")
+    if params[:departure_airport].present?
+      @flight_search_results = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND departure_date LIKE ?",
+         params[:departure_airport], params[:arrival_airport], params[:departure_date] + "%")
+    end
   end
 end
