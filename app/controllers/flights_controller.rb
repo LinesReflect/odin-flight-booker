974c5ebb482a3a52e9d0de4a031eb963773cb49d
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-03-20 12:04:50 -0500

    Add action that finds flights based on the search parameters

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @flight_search_results = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND departure_date LIKE ?",
+       params[:departure_airport], params[:arrival_airport], params[:departure_date] + "%")
   end
 end
