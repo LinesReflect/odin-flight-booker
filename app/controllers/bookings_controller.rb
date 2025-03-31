714dbc087a75bd8080746238f4e1d567cdf9aa9f
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-03-31 12:25:31 -0500

    Update the new method to build a passenger_count amount of passengers for a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,9 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
-    @passenger_count = params[:passenger_count]
+    @passenger_count = params[:passenger_count].to_i
+    @passenger_count.times do
+      @passenger = @booking.passengers.build
+    end
   end
 end
