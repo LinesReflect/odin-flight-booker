6cacba37c80692cc5dac01ac3038ca92fc0b15c6
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-04-02 14:04:20 -0500

    Create the create and show methods, and setup strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,24 @@ class BookingsController < ApplicationController
       @passenger = @booking.passengers.build
     end
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save!
+      redirect_to @booking
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.expect(booking: [ :flight_id, passengers_attributes: [ [ :name, :email ] ] ])
+  end
 end
