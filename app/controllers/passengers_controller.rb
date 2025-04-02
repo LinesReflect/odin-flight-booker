ac081aa96f3bbb702f5c335cd75c44741c1ae98b
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-04-02 14:03:15 -0500

    Create the create action and set up strong params

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,16 @@ class PassengersController < ApplicationController
   def new
     @passenger = Passenger.new
   end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    @passenger.save
+  end
+
+  private
+
+  def passenger_params
+    params.expect(passenger: [ :name, :email ])
+  end
 end
