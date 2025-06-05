c1e0c8b9252d21eb617a25e9b5419ac5635f52af
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-06-05 11:35:32 -0500

    Allow for additional passangers to be added to the booking form

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,4 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     
+    static targets = [ "addPassengerButton", "passengerList", "template" ]
+
+    static values = { passengerSpotsAvailable: Number }
+
+    connect() {
+        this.passengerSpotsAvailableValue = 4 - this.passengerListTarget.getElementsByTagName("li").length
+        this.toggleAddPassengerButton()
+    }
+
+    toggleAddPassengerButton() {
+        if (this.passengerSpotsAvailableValue > 0) {
+            this.addPassengerButtonTarget.hidden = false
+        } else {
+            this.addPassengerButtonTarget.hidden = true
+        }
+    }
+
+    addPassenger() {
+        this.passengerListTarget.insertAdjacentHTML("beforeend", this.#templateContent)
+        this.passengerSpotsAvailableValue--
+        this.toggleAddPassengerButton()
+    }
+
+    // private
+
+    get #templateContent() {
+        return this.templateTarget.innerHTML.replace(/__INDEX__/g, Date.now())
+    }
+
 }
