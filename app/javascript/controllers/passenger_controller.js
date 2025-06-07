cae80a38401657bb5af9b1c52de9df4c6ec1c49d
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-06-07 05:02:01 -0500

    Allow for passenger info input fields to be deleted

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     
-    static targets = [ "addPassengerButton", "passengerList", "template" ]
+    static targets = [ "addPassengerButton", "removePassengerButton", "passengerList", "template" ]
 
     static values = { passengerSpotsAvailable: Number }
 
     connect() {
         this.passengerSpotsAvailableValue = 4 - this.passengerListTarget.getElementsByTagName("li").length
         this.toggleAddPassengerButton()
+        this.toggleRemovePassengerButton()
     }
 
     toggleAddPassengerButton() {
@@ -18,10 +19,29 @@ export default class extends Controller {
         }
     }
 
+    toggleRemovePassengerButton() {
+        this.removePassengerButtonTargets.forEach(btn => {
+            if (this.passengerSpotsAvailableValue >= 3) {
+                btn.hidden = true
+            }else {
+                btn.hidden = false
+            }
+        });
+    }
+
     addPassenger() {
         this.passengerListTarget.insertAdjacentHTML("beforeend", this.#templateContent)
         this.passengerSpotsAvailableValue--
         this.toggleAddPassengerButton()
+        this.toggleRemovePassengerButton()
+    }
+
+    removePassenger(event) {
+        let li = event.currentTarget.parentElement.parentElement
+        li.parentElement.removeChild(li)
+        this.passengerSpotsAvailableValue++
+        this.toggleAddPassengerButton()
+        this.toggleRemovePassengerButton()
     }
 
     // private
